d9756444522465cd266927a1f40cf431ff9ed20e
Daninogamer <88000948+Daninogamer@users.noreply.github.com>
2022-06-12 17:32:13 +0200

    stat

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ client.categories = fs.readdirSync("./commands/");
 client.once('ready', () => {
     console.log(`${client.user.username} ✅`);
 })
-    
-
 
 
 
@@ -281,6 +279,46 @@ client.on("messageReactionRemove", async function (messageReaction, user) {
         
     }
 })
+
+  client.on("message", message => {
+        if (message.content == ">>>>>>") {
+            var embed = new MessageEmbed() //Crea il tuo embed o messaggio normale
+                .setTitle("VERIFICA")
+                .setDescription("Salve <@&985549568492056626>!\n **Per entrare a far parte del server premi la reazione ✅ che si trova qua sotto!**")
+    
+            message.channel.send(embed)
+                .then(msg => {
+                    //Inserire tutte le reazioni che si vogliono
+                    msg.react("✅")
+                })
+        }
+    })
+    //Quando viene cliccata una reazione
+    client.on("messageReactionAdd", async function (messageReaction, user) {
+        if (user.bot) return //Le reaction dei bot verranno escluse
+    
+        if (messageReaction.message.partial) await messageReaction.message.fetch();
+    
+        if (messageReaction.message.id == "985561084406685769") { //Settare id messaggio
+            if (messageReaction._emoji.name == "✅") {
+                var utente = messageReaction.message.guild.members.cache.find(x => x.id == user.id);
+                utente.roles.add("984868027361271828"); //Settare ruolo
+            }
+        }
+    })
+    //Quando viene rimossa una reazione
+    client.on("messageReactionRemove", async function (messageReaction, user) {
+        if (user.bot) return
+    
+        if (messageReaction.message.partial) await messageReaction.message.fetch();
+    
+        if (messageReaction.message.id == "985561084406685769") { //Settare id messaggio
+            if (messageReaction._emoji.name == "✅") {
+                var utente = messageReaction.message.guild.members.cache.find(x => x.id == user.id);
+                utente.roles.remove("985549568492056626"); //Settare ruolo
+            }
+        }
+    })
     
 //member log
 client.on("guildMemberUpdate", (oldMember, newMember) => {
